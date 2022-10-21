5c43a2e9b7771d4cc0ec1841021daf9601a79e99
Kedar Agate <kedar.agate@gmail.com>
2022-10-21 06:46:25 +0530

    all http methods working

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const random = require("random");
 const mongoose = require("mongoose");
-const cors=require("cors");
-
+const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI);
@@ -23,10 +22,7 @@ db.once("open", () => {
   console.log("db connection successful");
 });
 
-
-
 const blogSchema = new mongoose.Schema({
-  id: Number,
   title: String,
   author: String,
   body: String,
@@ -43,10 +39,10 @@ const blog = mongoose.model("blog", blogSchema);
 
 app.post("/api/blogs", (req, res) => {
   const { title, author, body } = req.body;
-  const id = random.int(1, 100000);
+
   const comments = [];
 
-  const newBlog = new blog({ title, author, body, id, comments });
+  const newBlog = new blog({ title, author, body, comments });
 
   newBlog
     .save()
@@ -77,34 +73,78 @@ app.get("/api/blogs", (req, res) => {
 });
 
 app.get("/api/blogs/:id", (req, res) => {
-  blog
-    .findById(req.params.id)
-    .then((data) => {
-      if (!data) {
-        res.status(400).send({ message: "Something went wrong" });
-      } else {
-        res.json(data);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  let id = req.params.id;
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    // Yes, it's a valid ObjectId, proceed with `findById` call.
+    blog
+      .findById(id)
+      .then((data) => {
+        if (!data) {
+          res.status(400).send({ message: "Something went wrong" });
+        } else {
+          res.json(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.status(400).send({ message: "Please enter correct id" });
+  }
 });
+
 app.put("/api/blogs/:id", (req, res) => {
+  let id = req.params.id;
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    blog
+      .findByIdAndUpdate(id)
+      .then((data) => {
+        if (!data) {
+          res.status(400).send({ message: "Something went wrong" });
+        } else {
+          let keysToUpdate = Object.keys(req.body);
+
+          keysToUpdate.forEach((key) => (data[key] = req.body[key]));
+          res.json(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.status(400).send({ message: "Please enter correct id" });
+  }
+});
+
+app.delete("/api/blogs", (req, res) => {
   blog
-    .findByIdAndUpdate(req.params.id)
+    .deleteMany({})
     .then((data) => {
       if (!data) {
-        res.status(400).send({ message: "Something went wrong" });
-      } else {
-        let keysToUpdate = Object.keys(req.body);
-        data[keysToUpdate] = req.body[keysToUpdate];
-        res.json(data);
+        res.status(400).send({ message: "Something went wrong." });
       }
+      res.json({
+        message: `Successfully deleted ${data.deletedCount} ${
+          deletedCount > 1 ? "blogs" : "blog"
+        }.`,
+      });
     })
-    .catch((err) => {
-      console.log(err);
+    .catch((err) => res.status(500).send({ message: err }));
+});
+
+app.delete("/api/blogs/:id", (req, res) => {
+  id = req.params.id;
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    blog.findByIdAndDelete(id).then((data) => {
+      if (!data) {
+        res.status(400).send({ message: "Something went wrong." });
+      } else {
+        res.send({ message: `Successfully deleted blog ${req.body.title}` });
+      }
     });
+  } else {
+    res.status(400).send({ message: "Please enter correct id" });
+  }
 });
 
 app.listen(process.env.PORT, () => {
