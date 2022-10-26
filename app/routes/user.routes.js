520105884b5469ddfb96ecb5c6a8f005f9a6fdbf
Kedar Agate <kedar.agate@gmail.com>
2022-10-26 17:35:10 +0530

    removed cors from user routes and again trying cors in server.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,9 +3,8 @@ const userController = require(path.join(
   __dirname,
   "../controllers/userController"
 ));
-const cors = require("cors");
 
 module.exports = (app) => {
-  app.post("/api/register", cors(), userController.register);
-  app.post("/api/login", cors(), userController.login);
+  app.post("/api/register", userController.register);
+  app.post("/api/login", userController.login);
 };
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.options("*", cors());
 
 app.use(bodyParser.json());
 
