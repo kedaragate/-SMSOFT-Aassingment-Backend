ec5689bbe9e521ec4925df54f649e8710d2e357a
Kedar Agate <kedar.agate@gmail.com>
2022-10-26 17:13:57 +0530

    cors

--- server.js
+++ server.js
@@ -12,17 +12,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use((req, res, next) => {
-  //allow access from every, elminate CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.removeHeader("x-powered-by");
-  //set the allowed HTTP methods to be requested
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  //headers clients can use in their requests
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //allow request to continue and be handled by routes
-  next();
-});
+app.use(cors());
 
 app.use(bodyParser.json());
 
